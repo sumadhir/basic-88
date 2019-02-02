c5ed9a30b87d24a947d6399d9993bdc58c799bde
sumadhir <sumadhir123@gmail.com>
2019-02-02 08:19:01 -0800

    Basic 88 1st commit

---
+++ myscript.js
@@ -0,0 +1,6 @@
+  var myName = prompt("Please Enter your Name?")
+  document.getElementById("myTitle").innerHTML= myName;
+  if(myName=="Sudheer"){
+    document.getElementById("myTitle").style.color ="red"
+  }
+ 
